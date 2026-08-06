40ffc08ee35b865b3db9cf9572153b4fdbcff2db
ilterisYucel <ilterisYucel@users.noreply.github.com>
2026-08-06 10:40:57 +0000

    ci: deploy storybook [skip ci] df5c9154ef573ab0d81b8f176ceb8ed18c9581b2

---
+++ assets/RackCell.stories-BYvHifhv.js
@@ -0,0 +1,34 @@
+import"./rolldown-runtime-aKtaBQYM.js";import{t as e}from"./react-BLJmJXjR.js";import{t}from"./jsx-runtime-DKdBMi_L.js";import{S as n,g as r}from"./Geometry-DsFOYoGD.js";import{n as i}from"./CanvasRenderer-CSK-Cc-P.js";import{d as a,l as o,r as s,u as c}from"./src-aaxa-_K-.js";import{c as l,s as u}from"./factories-CCuBTyQA.js";var d=t();e(),o({Container:n,Graphics:i,Text:a,Sprite:r});var f={title:`Graphics/RackCell`,component:s,tags:[`autodocs`]},p=l(100),m={width:160,height:420,background:`#0f0f1a`},h=()=>(0,d.jsx)(`div`,{style:m,children:(0,d.jsx)(c,{width:160,height:420,background:986906,antialias:!1,resolution:1,children:(0,d.jsx)(s,{rack:u(`online`,`Charge`,{soc:75,voltage:48.2,current:12.5}),x:20,y:20,config:p,flowDirection:`Charge`})})}),g=()=>(0,d.jsx)(`div`,{style:m,children:(0,d.jsx)(c,{width:160,height:420,background:986906,antialias:!1,resolution:1,children:(0,d.jsx)(s,{rack:u(`online`,`Discharge`,{soc:30,voltage:46.1}),x:20,y:20,config:p,flowDirection:`Discharge`})})}),_=()=>(0,d.jsx)(`div`,{style:m,children:(0,d.jsx)(c,{width:160,height:420,background:986906,antialias:!1,resolution:1,children:(0,d.jsx)(s,{rack:u(`offline`,`Idle`,{soc:null}),x:20,y:20,config:p,flowDirection:`Idle`})})}),v=()=>(0,d.jsx)(`div`,{style:m,children:(0,d.jsx)(c,{width:160,height:420,background:986906,antialias:!1,resolution:1,children:(0,d.jsx)(s,{rack:u(`online`,`Idle`,{soc:72}),x:20,y:20,config:p,flowDirection:`Idle`})})}),y=()=>(0,d.jsx)(`div`,{style:m,children:(0,d.jsx)(c,{width:160,height:420,background:986906,antialias:!1,resolution:1,children:(0,d.jsx)(s,{rack:u(`online`,`Charge`,{soc:100}),x:20,y:20,config:p,flowDirection:`Charge`})})});h.__docgenInfo={description:``,methods:[],displayName:`OnlineCharging`},g.__docgenInfo={description:``,methods:[],displayName:`OnlineDischarging`},_.__docgenInfo={description:``,methods:[],displayName:`Offline`},v.__docgenInfo={description:``,methods:[],displayName:`Idle`},y.__docgenInfo={description:``,methods:[],displayName:`Full`},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`() => <div style={wrapper}>
+    <Application width={160} height={420} background={0x0f0f1a} antialias={false} resolution={1}>
+      <RackCell rack={createMockRack("online", "Charge", {
+      soc: 75,
+      voltage: 48.2,
+      current: 12.5
+    })} x={20} y={20} config={config} flowDirection="Charge" />
+    </Application>
+  </div>`,...h.parameters?.docs?.source}}},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`() => <div style={wrapper}>
+    <Application width={160} height={420} background={0x0f0f1a} antialias={false} resolution={1}>
+      <RackCell rack={createMockRack("online", "Discharge", {
+      soc: 30,
+      voltage: 46.1
+    })} x={20} y={20} config={config} flowDirection="Discharge" />
+    </Application>
+  </div>`,...g.parameters?.docs?.source}}},_.parameters={..._.parameters,docs:{..._.parameters?.docs,source:{originalSource:`() => <div style={wrapper}>
+    <Application width={160} height={420} background={0x0f0f1a} antialias={false} resolution={1}>
+      <RackCell rack={createMockRack("offline", "Idle", {
+      soc: null
+    })} x={20} y={20} config={config} flowDirection="Idle" />
+    </Application>
+  </div>`,..._.parameters?.docs?.source}}},v.parameters={...v.parameters,docs:{...v.parameters?.docs,source:{originalSource:`() => <div style={wrapper}>
+    <Application width={160} height={420} background={0x0f0f1a} antialias={false} resolution={1}>
+      <RackCell rack={createMockRack("online", "Idle", {
+      soc: 72
+    })} x={20} y={20} config={config} flowDirection="Idle" />
+    </Application>
+  </div>`,...v.parameters?.docs?.source}}},y.parameters={...y.parameters,docs:{...y.parameters?.docs,source:{originalSource:`() => <div style={wrapper}>
+    <Application width={160} height={420} background={0x0f0f1a} antialias={false} resolution={1}>
+      <RackCell rack={createMockRack("online", "Charge", {
+      soc: 100
+    })} x={20} y={20} config={config} flowDirection="Charge" />
+    </Application>
+  </div>`,...y.parameters?.docs?.source}}};var b=[`OnlineCharging`,`OnlineDischarging`,`Offline`,`Idle`,`Full`];export{y as Full,v as Idle,_ as Offline,h as OnlineCharging,g as OnlineDischarging,b as __namedExportsOrder,f as default};
---
+++ assets/react-C9fjN-h7.js
@@ -0,0 +1,1 @@
+var e={};throw Error(`Could not resolve "sync-ammo" imported by "@playcanvas/react". Is it installed?`);export{e as default};
