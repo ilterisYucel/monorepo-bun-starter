5e31ba7098412ef4503c8cf02a647dd689040674
ilterisYucel <ilterisYucel@users.noreply.github.com>
2026-08-11 12:39:25 +0000

    ci: deploy storybook [skip ci] 0721d530a2186224782206cea711b00419c33e81

---
+++ assets/PanelCard.stories-DGLHHQVq.js
@@ -0,0 +1,13 @@
+import"./rolldown-runtime-aKtaBQYM.js";import{t as e}from"./react-BLJmJXjR.js";import{t}from"./jsx-runtime-DKdBMi_L.js";import{S as n,g as r}from"./Geometry-DsFOYoGD.js";import{n as i}from"./CanvasRenderer-CSK-Cc-P.js";import{d as a,l as o,o as s,u as c}from"./src-DwhkAR7w.js";import{l}from"./factories-CCuBTyQA.js";var u=t();e(),o({Container:n,Graphics:i,Text:a,Sprite:r});var d={title:`Graphics/PanelCard`,component:s,tags:[`autodocs`]},f=l(10,10,60,100),p={step:50},m={width:100,height:140,background:`#0f0f1a`},h=()=>(0,u.jsx)(`div`,{style:m,children:(0,u.jsx)(c,{width:100,height:140,background:986906,antialias:!1,resolution:1,children:(0,u.jsx)(s,{pos:f,panelTemp:5,config:p})})}),g=()=>(0,u.jsx)(`div`,{style:m,children:(0,u.jsx)(c,{width:100,height:140,background:986906,antialias:!1,resolution:1,children:(0,u.jsx)(s,{pos:f,panelTemp:22,config:p})})}),_=()=>(0,u.jsx)(`div`,{style:m,children:(0,u.jsx)(c,{width:100,height:140,background:986906,antialias:!1,resolution:1,children:(0,u.jsx)(s,{pos:f,panelTemp:40,config:p})})});h.__docgenInfo={description:``,methods:[],displayName:`Cold`},g.__docgenInfo={description:``,methods:[],displayName:`Normal`},_.__docgenInfo={description:``,methods:[],displayName:`Hot`},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`() => <div style={wrapper}>
+    <Application width={100} height={140} background={0x0f0f1a} antialias={false} resolution={1}>
+      <PanelCard pos={pos} panelTemp={5} config={config} />
+    </Application>
+  </div>`,...h.parameters?.docs?.source}}},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`() => <div style={wrapper}>
+    <Application width={100} height={140} background={0x0f0f1a} antialias={false} resolution={1}>
+      <PanelCard pos={pos} panelTemp={22} config={config} />
+    </Application>
+  </div>`,...g.parameters?.docs?.source}}},_.parameters={..._.parameters,docs:{..._.parameters?.docs,source:{originalSource:`() => <div style={wrapper}>
+    <Application width={100} height={140} background={0x0f0f1a} antialias={false} resolution={1}>
+      <PanelCard pos={pos} panelTemp={40} config={config} />
+    </Application>
+  </div>`,..._.parameters?.docs?.source}}};var v=[`Cold`,`Normal`,`Hot`];export{h as Cold,_ as Hot,g as Normal,v as __namedExportsOrder,d as default};
